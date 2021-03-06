5d2cb658fadf8404173c270ced9a3f5eead8ac28
FernandaDSilva <fdsferna@gmail.com>
2021-03-05 21:38:07 -0300

    fix bug on crud

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -1,53 +1,73 @@
-const { age, date } = require('../../lib/utils')
+const { age, date } = require("../../lib/utils");
+const Member = require("../models/Member");
 
-module.exports ={
-  index(req,res) {
-  return res.render("members/index")
+module.exports = {
+  index(req, res) {
+    Member.all(function (members) {
+      return res.render("members/index", { members });
+    });
   },
 
-  create(req,res) {
-  return res.render('members/create')
+  create(req,res){
+    Member.instructorsSelectOptions(function(options){
+        return res.render ('members/create', { instructorOptions: options })
+    })
   },
 
-  post(req,res) {
-    const keys = Object.keys(req.body)
- 
-    for(key of keys) {
-      if(req.body[key] == ""){
-      return res.send("Please, fill all fields")   
+  post(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Please, fill all fields");
+      }
+    }
+    Member.create(req.body, function (member) {
+      return res.redirect(`/members/${member.id}`);
+    });
+  },
+
+  show(req, res) {
+    Member.find(req.params.id, function (member) {
+      if (!member) return res.send("Member not found");
+
+      member.birth = date(member.birth).birthDay
+      
+
+      return res.render("members/show", { member });
+    });
+  },
+
+  edit(req, res) {
+    Member.find(req.params.id, function (member) {
+      if (!member) return res.send("Member not found");
+
+      member.birth = date(member.birth).iso;
+
+      Member.instructorsSelectOptions(function(options){
+        return res.render ('members/edit', { member, instructorOptions: options })
+    })
+
+    });
+  },
+
+  put(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Please, fill all fields");
       }
     }
 
-        
-    return 
+    Member.update(req.body, function () {
+      return res.redirect(`members/${req.body.id}`);
+    });
   },
 
-  show(req,res) {
-  return
-
+  delete(req, res) {
+    Member.delete(req.body.id, function () {
+      return res.redirect(`/members`);
+    });
   },
-
-  edit(req,res) {
-   
-  return 
-  },
-
-  put(req,res) {
-
-    const keys = Object.keys(req.body)
- 
-    for(key of keys) {
-      if(req.body[key] == ""){
-      return res.send("Please, fill all fields")   
-      }
-    }
-
-    return
-  },
-
-  delete(req,res) {
-    
-    return
-  }
-}
-
+};
--- src/app/models/Instructor.js
+++ src/app/models/Instructor.js
@@ -1,71 +1,62 @@
-const { age, date } = require('../../lib/utils')
 const db = require('../../config/db')
-
-
+const { date } = require('../../lib/utils')
 
 module.exports = {
-
     all(callback) {
-        db.query(`SELECT * FROM instructors`, function(err,results){
-            if(err) throw "Database Error!"
 
-            callback(results.rows)
-            
-          })
+        // ORDER BY para ordernar os dados
+        db.query(`
+            SELECT instructors.*, count(members) AS total_students 
+            FROM instructors
+            LEFT JOIN members ON (instructors.id = members.instructor_id)
+            GROUP BY instructors.id
+            ORDER BY total_students DESC
+            `, function (err, results){
+                if(err) throw `Database error! ${err}`    
+                callback(results.rows)
+        })
+
     },
-
-    create(data, callback){
-
+    create (data, callback) {
+        // inserindo informações na tabela no banco de dados
         const query = `
-        INSERT INTO instructors (
-          name,
-          avatar_url,
-          gender,
-          services,
-          birth,
-          created_at      
-        ) VALUES ($1, $2, $3, $4, $5, $6)
-        RETURNING id
-      `
-      const values = [
-        data.name,
-        data.avatar_url,
-        data.gender,
-        data.services,
-        date(data.birth).iso,
-        date(Date.now()).iso
-      ]
-  
-      db.query(query, values, function(err, results){
-        if(err)throw "Database Error!"
-  
-        callback(results.rows[0])
-  
-      })
+            INSERT INTO instructors (
+                name, 
+                avatar_url,
+                gender,
+                services,
+                birth,
+                created_at
+            ) VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING id
+        `
 
-    },
+        // esse array sera responsavel por substituir os placeholder ($1, $2...)
+        const values = [
+            data.name,
+            data.avatar_url,
+            data.gender,
+            data.services,
+            date(data.birth).iso,
+            date(Date.now()).iso
+        ]
 
-    find(id, callback) {
-
-        db.query(`SELECT * 
-        FROM instructors 
-        WHERE id = $1`, [id], function(err,results) {
-            if(err) throw `Database Error! ${err}`
-
+        db.query(query, values, function(err, results) {
+            if(err) throw `Database error! ${err}`            
             callback(results.rows[0])
-            
-          })
+        })
     },
     update(data, callback) {
         const query = `
-        UPDATE instructors SET
-            avatar_url=($1),
-            name=($2),
-            birth=($3),
-            gender=($4),
-            services=($5)
-        WHERE id = $6
+            UPDATE instructors SET
+                avatar_url=($1),
+                name=($2),
+                birth=($3),
+                gender=($4),
+                services=($5)
+            WHERE id = $6
         `
+
         const values = [
             data.avatar_url,
             data.name,
@@ -75,12 +66,11 @@ module.exports = {
             data.id
         ]
 
-        db.query(query, values,function(err, results) {
-            if(err) throw `Database Error! ${err}`
+        db.query(query, values, function(err, results){
+            if(err) `Database error! ${err}`              
             callback()
         })
     },
-
     delete(id, callback) {
         db.query(`
             DELETE 
@@ -89,6 +79,6 @@ module.exports = {
                 if(err) throw `Database error! ${err}`             
                 return callback()
         })
-    }
-
+    },
+  
 }
